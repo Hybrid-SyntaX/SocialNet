f86dcd99a6f98154030e8af6f9cc50e73459260a
Abbas <hybrid_syntax@hotmail.com>
2018-08-09 13:29:08 +0430

    intial

---
+++ SocialNet/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNet.Models
+{
+    public class Post
+    {
+        [Key] public Guid Id { set; get; }
+        public string Title { set; get; }
+        public string Content { set; get; }
+    }
+}
